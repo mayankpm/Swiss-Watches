7560f6b07696b6f0c22e6c93925bd69ad7c2b3bd
your_github_username <your_github_email>
2024-02-02 09:07:18 +0530

    fixing reloading

--- Lux_Watches/src/Components/WatchCollection/CardsCollection.jsx
+++ Lux_Watches/src/Components/WatchCollection/CardsCollection.jsx
@@ -1,40 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom"
-// // import './CardsCollection.css'
-
-// import {Card, ap_1159, cactus_jack, royaloak_perpetual, carrera_date, carrera_porsche, carrera, daydate_green, daydate_gold, daydate_blue,patek_green,patek_teal,patek_blue} from "./images.jsx" 
-
-
-// export default function CardsCollection() {
-
-//     // Navigate to watches
-//     const navigate = useNavigate();
-
-//     const navigateToWatch = (imageSrc) => {
-//         // window.scrollTo({
-//         //     top: 0
-//         //   });
-//         navigate('/watch',{ state: { image: imageSrc } });
-//     }
-
-//     return(
-//         <div className="container gap-2 m-auto justify-center items-center text-black grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-//             <div className="card card-1" onClick={() => navigateToWatch(ap_1159)}> <Card pfp={ap_1159} name="Code 11.59 by Audemars Piguet"  desc="Manual winding ultralat" brand="Audemars Piguet"/></div>
-//             <div className="card card-2" onClick={() => navigateToWatch(royaloak_perpetual)}><Card pfp={royaloak_perpetual} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-3" onClick={() => navigateToWatch(cactus_jack)}><Card pfp={cactus_jack} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-4" onClick={() => navigateToWatch(carrera_date)}><Card pfp={carrera_date} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-5" onClick={() => navigateToWatch(carrera_porsche)}><Card pfp={carrera_porsche} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-6" onClick={() => navigateToWatch(carrera)}><Card pfp={carrera} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-7" onClick={() => navigateToWatch(daydate_green)}><Card pfp={daydate_green} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-8" onClick={() => navigateToWatch(daydate_gold)}><Card pfp={daydate_gold} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-9" onClick={() => navigateToWatch(daydate_blue)}><Card pfp={daydate_blue} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-10" onClick={() => navigateToWatch(patek_green)}><Card pfp={patek_green} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-11" onClick={() => navigateToWatch(patek_teal)}><Card pfp={patek_teal} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//             <div className="card card-12" onClick={() => navigateToWatch(patek_blue)}><Card pfp={patek_blue} name="Code 11.59 by Audemars Piguet" desc="Manual winding ultralat" brand="Rolex"/></div>
-//         </div>
-//     )
-// }
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -109,7 +72,9 @@ import.meta.env.VITE_BACKEND_URL
       <div className="container gap-2 m-auto justify-center items-center text-black grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
         {watches.map((watch, index) => (
           <div className={`card card-${index + 1}`} key={index} onClick={() => navigateToWatch(watch)}>
-            <Card pfp_hover={nameToVariableMap1[watch.name]} pfp={nameToVariableMap[watch.name]} name={watch.name} desc={watch.description} brand={watch.image} />
+            {watch.name && watch.description && watch.image && (
+              <Card pfp_hover={nameToVariableMap1[watch.name]} pfp={nameToVariableMap[watch.name]} name={watch.name} desc={watch.description} brand={watch.image} />
+            )}
           </div>
         ))}
       </div>
