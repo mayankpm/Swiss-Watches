d24371d0b4a278fd55c2edfd403b1325953ada06
your_github_username <your_github_email>
2024-02-02 13:32:01 +0530

    Revert "fixing reloading"
    This reverts commit 59146aed8bff1b59a7a5e2521494f0f22f55b354.

--- Lux_Watches/src/Components/WatchCollection/CardsCollection.jsx
+++ Lux_Watches/src/Components/WatchCollection/CardsCollection.jsx
@@ -11,6 +11,7 @@ import {ap_1159_h, cactus_jack_h, royaloak_perpetual_h, carrera_date_h, carrera_
 export default function CardsCollection() {
   const [watches, setWatches] = useState([]);
   const navigate = useNavigate();
+import.meta.env.VITE_BACKEND_URL
   const fetchWatches = async () => {
     try {
       const response = await axios.get(import.meta.env.VITE_BACKEND_URL + "/auth/watches/");
@@ -29,7 +30,7 @@ export default function CardsCollection() {
 
   useEffect(() => {
     fetchWatches();
-  }, [import.meta.env.VITE_BACKEND_URL]);
+  }, []);
 
   const nameToVariableMap = {
     "Code 11.59 by Audemars Piguet": ap_1159,
