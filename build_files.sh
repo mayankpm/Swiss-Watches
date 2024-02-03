ef8cb9a58fae43d0446b2aad915cb03ffae03789
your_github_username <your_github_email>
2024-02-03 23:52:02 +0530

    folder structure

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-echo "DJANGO BUILD STARTED"
-python3 manage.py collectstatic --noinput --clear
-echo "DJANGO BUILD ENDED"
-
-echo "VITE BUILD STARTED"
-cd Lux_Watches
-npm run build
-echo "VITE BUILD ENDED"
